29fc3f6aea057a8521287b1c822d6d7075e19b68
Meshmanuu <meshanyangu@gmail.com>
2024-03-31 20:15:05 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+        function superbowlWin(record) {
+    for (const game of record) {
+      if (game.result === "W") {
+        return game.year;
+      }
+    }
+    return undefined;
+  }
+  
